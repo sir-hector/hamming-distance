5ddee28682c815409b97d55275d3fced4764d1bc
sir-hector <krauskarol7@gmail.com>
2022-12-09 13:41:27 +0100

    add implementation and test- whole song

--- ChristmasCarol.py
+++ ChristmasCarol.py
@@ -1,2 +1,18 @@
+things = ["", " and a Partridge in a Pear Tree.", " two Turtle Doves,", " three French Hens,", " four Calling Birds,",
+          " five Gold Rings,", " six Geese-a-Laying,", " seven Swans-a-Swimming,", " eight Maids-a-Milking,",
+          " nine Ladies Dancing,", " ten Lords-a-Leaping,", " eleven Pipers Piping,", " twelve Drummers Drumming,"]
+numbers = ["", "first", "second", "third", "fourth", "fifth", "sixth", "seventh",
+           "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+
+
 def generateSingle(number):
-    return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree"
+    base = " day of Christmas my true love gave to me:"
+    if number == 1:
+        return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree"
+
+    response = "On the " + numbers[number] + base
+
+    for i in range(number, 0, -1):
+        response = response + things[i]
+
+    return response
--- christmas_test.py
+++ christmas_test.py
@@ -4,4 +4,33 @@ from ChristmasCarol import generateSingle
 
 class ChristmasTest(unittest.TestCase):
     def test_first_werse(self):
-        self.assertEqual(generateSingle(1), "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree")
+        self.assertEqual(generateSingle(1),
+                         "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree")
+
+    def test_second_day(self):
+        self.assertEqual(generateSingle(2),
+                         "On the second day of Christmas my true love gave to me: two Turtle Doves, " +
+                         "and a Partridge in a Pear Tree.")
+
+    def test_third_day(self):
+        self.assertEqual(generateSingle(3),
+                         "On the third day of Christmas my true love gave to me: three French Hens, " +
+                         "two Turtle Doves, and a Partridge in a Pear Tree.")
+
+    def test_fourth_day(self):
+        self.assertEqual(generateSingle(4),
+                         "On the fourth day of Christmas my true love gave to me: four Calling Birds," +
+                         " three French Hens, " +
+                         "two Turtle Doves, and a Partridge in a Pear Tree.")
+
+    def test_fifth_day(self):
+        self.assertEqual(generateSingle(5),
+                         "On the fifth day of Christmas my true love gave to me: five Gold Rings, " +
+                         "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+
+    def test_eleven_day(self):
+        self.assertEqual(generateSingle(11),
+                         "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping," +
+                         " ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming," +
+                         " six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens," +
+                         " two Turtle Doves, and a Partridge in a Pear Tree.")
