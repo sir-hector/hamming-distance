0caf9010b2487e04b8697ba2a586c3f65a9f30cb
sir-hector <krauskarol7@gmail.com>
2022-12-17 20:16:06 +0100

    add implementation for carol

--- ChristmasCarol.py
+++ ChristmasCarol.py
@@ -6,6 +6,8 @@ numbers = ["", "first", "second", "third", "fourth", "fifth", "sixth", "seventh"
 
 
 def generateSingle(number):
+    if number == 0:
+        return
     base = " day of Christmas my true love gave to me:"
     if number == 1:
         return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree"
@@ -16,3 +18,21 @@ def generateSingle(number):
         response = response + things[i]
 
     return response
+
+
+def generateInRange(low, high):
+    if low > high:
+        raise ValueError("First number mest be lower than second")
+
+    if low < 1 or low > 12:
+        raise ValueError("params must be in range of 1-12")
+
+    result = ''
+    for i in range(low, high + 1, +1):
+        result += generateSingle(i) + '\n'
+
+    return result
+
+
+def generateWholeSong():
+    return generateInRange(1, 12)
--- christmas_test.py
+++ christmas_test.py
@@ -1,5 +1,6 @@
 import unittest
-from ChristmasCarol import generateSingle
+from assertpy import *
+from ChristmasCarol import generateSingle, generateInRange, generateWholeSong
 
 
 class ChristmasTest(unittest.TestCase):
@@ -34,3 +35,19 @@ class ChristmasTest(unittest.TestCase):
                          " ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming," +
                          " six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens," +
                          " two Turtle Doves, and a Partridge in a Pear Tree.")
+
+
+    def test_range_1_2(self):
+        assert_that(generateInRange(1, 3)).contains("first", "second", "third")
+
+    def test_range_8_9(self):
+        assert_that(generateInRange(8, 9)).contains("eight", "nine")
+
+    def test_range_8_9_not_contains(self):
+        assert_that(generateInRange(8, 9)).does_not_contain("ten", "eleven")
+
+    def test_range_1_2_not_contains(self):
+        assert_that(generateInRange(1, 3)).does_not_contain("fourth")
+
+    def test_whole_song(self):
+        assert_that(generateWholeSong()).contains("twelfth", "second", "third")
