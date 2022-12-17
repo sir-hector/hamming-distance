56f7e86b1caee6a2944018200f40fadf5d1e2359
sir-hector <krauskarol7@gmail.com>
2022-12-17 19:07:49 +0100

    add message to error in Hammming

--- Hamming.py
+++ Hamming.py
@@ -2,7 +2,7 @@ class hamming:
     def distance(self, first, second):
 
         if len(first) != len(second):
-            raise ValueError(".+")
+            raise ValueError("Different lengths")
 
         if len(first) == 0 and len(second) == 0:
             return 0
